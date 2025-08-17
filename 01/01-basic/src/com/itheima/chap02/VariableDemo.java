c166152e7182b47c738436ed598856b032fa5587
icestone <killicestone@126.com>
2025-08-17 23:52:02 +0800

    chore: 添加 IntelliJ IDEA 项目配置和忽略文件
    - 新增 .idea 目录和相关配置文件
    - 添加 .gitignore 文件，定义项目中需要忽略的文件和目录
    - 创建模块配置文件 01-basic.iml
    - 设置部署配置 deployment.xml
    -配置语言注入 IntelliLang.xml

---
+++ 01/01-basic/src/com/itheima/chap02/VariableDemo.java
@@ -0,0 +1,14 @@
+package com.itheima.chap02;
+
+public class VariableDemo {
+    public static void main(String[] args) {
+        String name = "张三";
+        int age = 18;
+        System.out.println(name);
+        System.out.println(age);
+
+        age = 99;
+        System.out.println(age);
+    }
+
+}
